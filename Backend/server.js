429d55408f446e68c291dcbcc3dc89c4bc77236a
Mohammed Bilal <bilu02491@gmail.com>
2026-04-08 19:07:28 +0530

    NewUpdate

--- Backend/server.js
+++ Backend/server.js
@@ -52,7 +52,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://coopmart-frontend.onrender.com",
   credentials: true
 }));
 
